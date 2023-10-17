05523a22ed9ffc455aa495cec5e055fde75cb4ee
MohAlkurdi <imohkurdi@gmail.com>
2023-10-17 17:48:43 +0300

    add countdown

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import dayjs from "dayjs";
+import CountDown from "react-native-countdown-component";
 
 export default function App() {
+  const graduationDate = dayjs("2023-11-16");
+  const today = dayjs();
+  const diffDate = graduationDate.diff(today, "s");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.pages}>
+      <Text>Graduation Countdown</Text>
+      <Text>Wednesday, November 16th</Text>
+      <CountDown
+        until={diffDate / 1000}
+        size={30}
+        onFinish={() => alert("Graduated!")}
+        digitStyle={{ backgroundColor: "#FFF" }}
+        digitTxtStyle={{ color: "#fb7185" }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pages: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
